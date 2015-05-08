92abce634a75fe5fcb7797cc4f2630c4e85771a8
susan <slunn@csh.rit.edu>
2015-05-08 11:54:12 -0400

    anova

--- benchmarkerANOVA.R
+++ benchmarkerANOVA.R
@@ -5,4 +5,5 @@ library(jsonlite)
 
 data1 <- fromJSON(paste(getwd(), "json.json", sep="/")) 
 
+##FIT ANOVA AND PRINT RESULTS
 boxplot(data1, xlab="Executables", ylab="Time(means)", col=c("lightblue","lightgreen"))
